a112ffd02faf8eade5cd1c9020d5406345384aeb
torohkiss <torohkiss@yahoo.com>
2024-11-21 16:34:19 +0000

    changed return value from float to int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,7 +5,7 @@
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """type-annotated function floor which
     takes a float n as argument and returns the
     floor of the float."""
