ea8591bac9313d756769787c51b9d4022ea9a0df
torohkiss <torohkiss@yahoo.com>
2024-11-21 16:30:58 +0000

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A module that does Basic annotations - floor"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """type-annotated function floor which
+    takes a float n as argument and returns the
+    floor of the float."""
+
+    return math.floor(n)
