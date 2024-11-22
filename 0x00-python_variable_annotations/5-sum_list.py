3c25c02a708078af5b2d67b1e1aff2896f52ec49
torohkiss <torohkiss@yahoo.com>
2024-11-22 05:41:19 +0000

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,15 @@
 """A module that sums Complex
 types - list of floats"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
-    summ: float = 0
+
+def sum_list(input_list: List[float]) -> float:
+    """a type-annotated function sum_list which takes
+    a list input_list of floats as argument
+    and returns their sum as a float"""
+
+    summ: float = 0.0
 
     for x in input_list:
         summ += x
